6b771661dd79f6f7d7fe67efc597aa177165435f
Bartek Zlotnik <macaron@Barteks-MacBook-Pro.local>
2023-10-12 11:09:36 +0200

    changed code structure to classes

--- reciever.py
+++ reciever.py
@@ -1,63 +1,77 @@
 import socket
 import json
 
-def get_public_ip():
-    try:
-        # Create a socket object
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        
-        # Connect to a remote server (does not actually send any data)
-        s.connect(("8.8.8.8", 80))
-        
-        # Get the local socket address, which includes the public IP
-        public_ip = s.getsockname()[0]
-        
-        return public_ip
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
 
-IP = get_public_ip()
-PORT = int(input("Please insert open port: "))
-
-print(IP)
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-server.bind((IP, PORT))
+class ServerSite():
     
-server.listen()
 
-client, addr = server.accept()
+    def __init__(self) -> None:
 
-metadata = json.loads(client.recv(1024).decode())
+        self.IP = self.get_public_ip()
+        print(self.IP)
+        self.PORT = self.get_port()
+        self.create_server()
+        self.recieving_file()
+     
+    def get_public_ip(self):
+        try:
 
-file_name = metadata["file_name"]
-file_size = metadata["file_size"]
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            
+            s.connect(("8.8.8.8", 80))
+            
+            public_ip = s.getsockname()[0]
+            print(public_ip)
+            return public_ip
+        
+        except Exception as e:
 
-print(file_name)
-print(file_size)
+            print(f"Error: {e}")
 
-file = open("e.txt", "wb")
+            return None
 
-done = False
 
-file_bytes = b""
+    def get_port(self):
 
-# progress = tqdm.tqdm(unit="B", unit_scale=True, 
-#                      unit_divisor=1024, total=int(file_size))
+        port = int(input("Please insert open port: "))
 
-while not done:
+        return port
+    
 
-    data = client.recv(1024)
+    def create_server(self):
 
-    if file_bytes[-5:] == b"<END>":
-        done = True
-    else:
-        file_bytes += data
-        # progress.update(1024)
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.IP, self.PORT))
 
-file.write(file_bytes[:-5])
+    
+    def recieving_file(self):
 
-file.close()
-client.close()
-server.close()
+        self.server.listen()
+        self.client, self.addr = self.server.accept()
+
+        metadata = json.loads(self.client.recv(1024).decode())
+        recieved_file_name = metadata["file_name"]
+        recieved_file_size = metadata["file_size"]
+
+        saved_file_name = str(input("Please insert file name with correct file extension to save: "))
+
+        file_to_save = open(saved_file_name, "wb")
+        file_to_save_bytes = b""
+        done = False
+
+        while not done:
+
+            data = self.client.recv(1024)
+
+            if file_to_save_bytes[-5:] == b"<END>":
+                done = True
+
+            else:
+                file_to_save_bytes += data
+
+        file_to_save.write(file_to_save_bytes[:-5])
+        file_to_save.close()
+        self.client.close()
+        self.server.close()
+
+server = ServerSite()
--- sender.py
+++ sender.py
@@ -2,42 +2,52 @@ import socket
 import os
 import json
 
-def get_public_ip():
-    try:
-        # Create a socket object
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+class SenderSite():
+
+
+    def __init__(self) -> None:
+
+        self.IP = self.get_public_ip()
+        self.PORT = self.get_port()
+        self.ADDR = (self.IP, self.PORT)
+        self.PACKAGE_SIZE = 1024
+        self.FORMAT = "utf-8"
+
+        self.creating_connection()
+        self. sending_file()
         
-        # Connect to a remote server (does not actually send any data)
-        s.connect(("8.8.8.8", 80))
+    def get_public_ip(self):
+
+        ip = str(input("Please insert reciever IP address: "))
+
+        return ip
         
-        # Get the local socket address, which includes the public IP
-        public_ip = s.getsockname()[0]
-        
-        return public_ip
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
 
-IP = str(input("Please insert reciever IP address: "))
-PORT = int(input("Please insert open port: "))
-ADDR = (IP, PORT)
-SIZE = 1024
-FORMAT = "utf-8"
+    def get_port(self):
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        port = int(input("Please insert open port: "))
 
-client.connect((IP, PORT))
-print(IP)
-file_name = "file.txt"
-file_size = os.path.getsize("file.txt")
+        return port
+    
 
-metadata = {"file_name": file_name, "file_size": file_size}
-client.send(json.dumps(metadata).encode())
+    def creating_connection(self):
 
-sfile = open("file.txt", "rb")
-data = sfile.read()
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(self.ADDR)
 
 
-client.sendall(data)
-client.send(b"<END>")
-client.close()
+    def sending_file(self):
+
+        file_name = str(input("Please insert path or local filename to be send with file extension: "))
+        file_size = os.path.getsize(file_name)
+
+        metadata = {"file_name": file_name, "file_size": file_size}
+        self.client.send(json.dumps(metadata).encode())
+
+        file_to_send = open(file_name, "rb")
+        data = file_to_send.read()
+
+        self.client.sendall(data)
+        self.client.send(b"<END>")
+        self.client.close()
