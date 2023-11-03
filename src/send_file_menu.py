7764abd5e5b0332f32002f34675c57bd0f8bbeef
Bartosz Złotnik <bartekzlotnik@gmail.com>
2023-11-03 15:49:19 +0100

    Added documentation

--- src/send_file_menu.py
+++ src/send_file_menu.py
@@ -46,33 +46,53 @@ class SendFile(QMainWindow):
 
 
     def getting_uploading_speed(self):
-
+        """
+        Get the current uploading speed using the Speedtest library and update the 'uploading_speed' attribute.
+        """
         st = speedtest.Speedtest()
         self.uploading_speed= st.upload()
         self.uploading_speed = round(self.uploading_speed) / 2
 
 
     def updating_progress_bar(self, value: int):
+        """
+        Update the sending progress bar with the current value.
 
+        Parameters:
+        - value (int): Current progress value.
+        """
         self.sending_progress_bar.setValue(value)
 
 
     def close_and_popup(self, filename):
-        
+        """
+        Close the application window and display a popup notification with the sent filename.
+
+        Parameters:
+        - filename (str): The name of the sent file.
+        """
         self.close()
         self.popup_sent.setting_labels("File has been sent", filename)
         self.popup_sent.show()
 
 
     def disabling_buttons(self):
-
+        """
+        Disable UI buttons to prevent user interaction during file sending.
+        """
         self.set_connection_button.setEnabled(False)
         self.choose_dir_button.setEnabled(False)
         self.send_file_button.setEnabled(False)
 
 
     def updating_progress_label_value(self, value: int, max_value: int):
-        
+        """
+        Update the progress label with current and maximum values in MB.
+
+        Parameters:
+        - value (int): Current progress value.
+        - max_value (int): Maximum progress value.
+        """
         MB_value = round(float(value / 1048576), 2)
         max_MB_value = round(float(max_value / 1048576), 2)
         text = f'{MB_value} / {max_MB_value} MB'
@@ -80,7 +100,12 @@ class SendFile(QMainWindow):
 
 
     def generating_aes_key(self):
+        """
+        Generate an AES encryption object based on the user-entered key.
 
+        Returns:
+        - bool: True if encryption is successful, False if the key is invalid.
+        """
 
         key = self.aes_textedit.toPlainText()
 
@@ -101,7 +126,9 @@ class SendFile(QMainWindow):
 
 
     def opening_file_dialog(self):
-
+        """
+        Open a file dialog to select the file to send.
+        """
         file_dialog = QFileDialog()
         file_dialog.setNameFilter("All Files (*)")
         self.selected_file = file_dialog.getOpenFileName(None, "Open File", "", "All Files (*)")
@@ -126,7 +153,11 @@ class SendFile(QMainWindow):
 
 
     def sending_file(self):
+        """
+        Set up the server connection using the provided IP address and port.
 
+        Validates the IP and port, and sets the connection status accordingly.
+        """
         try:
 
             if self.validity is True and os.path.isfile(self.selected_file) == True:
